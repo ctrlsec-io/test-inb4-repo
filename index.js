61f4bd26b99ca80ce1f0ac3d3ef65241f6b5c24c
doomholderz <josephbucket@gmail.com>
2023-09-23 17:19:33 +0100

    adding vulnerable js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-const foo = "bar"
-console.log(foo)
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the vulnerable Express app!');
+});
+
+app.post('/search', (req, res) => {
+  const searchTerm = req.body.searchTerm;
+  const query = `SELECT * FROM products WHERE name LIKE '%${searchTerm}%'`; // Vulnerable SQL query
+
+  // In a real application, you should never construct SQL queries like this
+  // This is just for demonstration purposes to create a Semgrep-flagged vulnerability
+
+  // Execute the query and return results...
+  res.send(`Searching for: ${searchTerm}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
