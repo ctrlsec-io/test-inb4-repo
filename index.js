e128c3a5848b5ab2e5a7dc356a3165a508c065d6
doomholderz <josephbucket@gmail.com>
2023-10-19 17:34:16 +0100

    testing bad express

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const jwt = require("jsonwebtoken")
+
+const generate_session = async () => {
+    const session_token = jwt.sign({ id: 1, client_id: 2 }, "secretValue", {
+        expiresIn: "24h"
+    })
+    return session_token
+}
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,7 +19,9 @@ app.get('/', (req, res) => {
 app.post('/search', (req, res) => {
   const searchTerm = req.body.searchTerm;
   const query = `SELECT * FROM products WHERE name LIKE '%${searchTerm}%'`; // Vulnerable SQL query
-
+  const session = generate_session()
+  console.log(session)
+  const secret_value = "password123"
   // In a real application, you should never construct SQL queries like this
   // This is just for demonstration purposes to create a Semgrep-flagged vulnerability
 
